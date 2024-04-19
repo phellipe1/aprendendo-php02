970a8e64c62948bbfff28d1e1b381aa86f5e7864
phellipe1 <phellipecardosooo@gmail.com>
2024-04-18 23:11:43 -0300

    Condições em php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php 
+        $a = 2;
+        $b = 23;
+        if($a > $b){
+            echo "a maior que b";
+        } else if($a == $b){
+            echo "a igual a b";
+        } else{
+            echo "b maior que a";
+        }
+    ?>
+</body>
+
+</html>
